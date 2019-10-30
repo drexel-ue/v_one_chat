141f30bc7b39324ceba63267de4d77feed8af05f
ikesh pack <packappsforyou@gmail.com>
2019-10-30 16:04:31 -0400

    Lock device into protrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:v_one_flutter_chat/utils.dart';
 import 'package:v_one_flutter_chat/views/chat.dart';
 import 'package:v_one_flutter_chat/views/landing.dart';
 import 'package:v_one_flutter_chat/views/login.dart';
 import 'package:v_one_flutter_chat/views/registration.dart';
 
-void main() => runApp(
-      MaterialApp(
-        title: 'Chat Demo',
-        theme: ThemeData.dark(),
-        initialRoute: 'LANDING',
-        routes: {
-          landingRoute: (BuildContext context) => Landing(),
-          registrationRoute: (BuildContext context) => Registration(),
-          loginRoute: (BuildContext context) => Login(),
-          chatRoute: (BuildContext context) => Chat(),
-        },
-      ),
-    );
+void main() async {
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+  runApp(
+    MaterialApp(
+      title: 'Chat Demo',
+      theme: ThemeData.dark(),
+      initialRoute: 'LANDING',
+      routes: {
+        landingRoute: (BuildContext context) => Landing(),
+        registrationRoute: (BuildContext context) => Registration(),
+        loginRoute: (BuildContext context) => Login(),
+        chatRoute: (BuildContext context) => Chat(),
+      },
+    ),
+  );
+}
