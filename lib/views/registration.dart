30cf95b2c4b91dfbf8e9382d8357a86d4c1739a9
ikesh pack <packappsforyou@gmail.com>
2019-10-30 15:59:43 -0400

    Layout register view

--- lib/views/registration.dart
+++ lib/views/registration.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:v_one_flutter_chat/auth_services/firebase_auth_service.dart';
 import 'package:v_one_flutter_chat/auth_services/user.dart';
 import 'package:v_one_flutter_chat/views/chat.dart';
+import 'package:v_one_flutter_chat/widgets/custom_button.dart';
 
 class Registration extends StatefulWidget {
   Registration({Key key}) : super(key: key);
@@ -31,6 +32,36 @@ class _RegistrationState extends State<Registration> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Hero(
+              tag: 'logo',
+              child: Container(
+                width: 100,
+                child: Image.asset('assets/logo.png'),
+              ),
+            ),
+            SizedBox(height: 40),
+            TextField(),
+            SizedBox(height: 40),
+            TextField(),
+            SizedBox(height: 40),
+            CustomButton(
+              text: 'Register',
+              callback: () async {
+                await registerUser();
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
