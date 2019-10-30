b079351c000d04d02b8eb994fd45e6f9d6e7c0be
ikesh pack <packappsforyou@gmail.com>
2019-10-30 17:12:04 -0400

    Add show loading animation to prevent double taps

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,4 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
 const landingRoute = 'LANDING';
 const loginRoute = 'LOGIN';
 const registrationRoute = 'REGISTRATION';
 const chatRoute = 'CHAT';
+
+void showLoading(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) => Center(
+      child: CircularProgressIndicator(),
+    ),
+  );
+}
