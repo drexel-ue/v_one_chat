cac8caf3f6a3e77c08c3483aeca85e5f2b8f5a6a
ikesh pack <packappsforyou@gmail.com>
2019-10-30 15:06:17 -0400

    Add named routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:v_one_flutter_chat/views/chat.dart';
 import 'package:v_one_flutter_chat/views/landing.dart';
+import 'package:v_one_flutter_chat/views/login.dart';
+import 'package:v_one_flutter_chat/views/registration.dart';
 
 void main() => runApp(
       MaterialApp(
         title: 'Chat Demo',
         theme: ThemeData.dark(),
-        home: Landing(),
+        initialRoute: 'LANDING',
+        routes: {
+          'LANDING': (BuildContext context) => Landing(),
+          'REGISTRATION': (BuildContext context) => Registration(),
+          'LOGIN': (BuildContext context) => Login(),
+          'CHAT': (BuildContext context) => Chat(),
+        },
       ),
     );
